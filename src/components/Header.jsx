44acc422f8894679d05e4dc031e604183c740777
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-02-25 15:25:28 -0500

    added Home link to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,10 @@ const Header = () => {
     <header className="w-full fixed top-0 p-2 md:p-4">
       <div className="flex justify-between items-center w-full md:space-x-96">
         <ThemeToggle />
-        <Link to="/recipes">Recipes</Link>
+        <div className="flex gap-x-4">
+          <Link to="/">Home</Link>
+          <Link to="/recipes">Recipes</Link>
+        </div>
       </div>
     </header>
   );
