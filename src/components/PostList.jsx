44d8fc332863d629dce61c81e684943291bdaa93
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-03-10 16:41:00 -0400

    initial posts styling

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -17,7 +17,12 @@ const PostList = ({ topic }) => {
     <ul>
       {posts.map((post) => (
         <li key={post._id}>
-          <p>{post.posts}</p>
+          <div className="flex flex-col border rounded shadow-lg bg-gray-50 w-screen md:w-200 text-black min-h-25">
+            <p className="p-2">{post.posts}</p>
+            <div className="mt-auto border-t border-gray-300">
+              <span>test</span>
+            </div>
+          </div>
         </li>
       ))}
     </ul>
--- src/pages/ForumPage.jsx
+++ src/pages/ForumPage.jsx
@@ -5,7 +5,7 @@ const ForumPage = () => {
   const { topic } = useParams();
 
   return (
-    <div className="mt-20">
+    <div className="flex  flex-col mt-20 items-center">
       <h1>{topic.charAt(0).toUpperCase() + topic.slice(1)} Forum</h1>
       <PostList topic={topic} />
     </div>
