7fe80da643c66299ed3c298adac1dce3e60f2338
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-03-11 16:36:30 -0400

    sort posts descending, added username to post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -15,16 +15,19 @@ const PostList = ({ topic }) => {
 
   return (
     <ul>
-      {posts.map((post) => (
-        <li key={post._id}>
-          <div className="flex flex-col border rounded shadow-lg bg-gray-50 w-screen md:w-200 text-black min-h-25 mb-2">
-            <p className="p-2">{post.post}</p>
-            <div className="mt-auto border-t border-gray-300">
-              <span>test</span>
+      {posts
+        .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) // sort descending based on createdAt
+        .map((post) => (
+          <li key={post._id}>
+            <div className="flex flex-col border rounded shadow-lg bg-gray-50 w-screen md:w-200 text-black min-h-25 mb-2">
+              <p className="px-2">{post.username}:</p>
+              <p className="px-2">{post.post}</p>
+              <div className="mt-auto border-t border-gray-300">
+                <span>test</span>
+              </div>
             </div>
-          </div>
-        </li>
-      ))}
+          </li>
+        ))}
     </ul>
   );
 };
