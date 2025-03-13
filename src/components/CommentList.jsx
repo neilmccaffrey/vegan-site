9a926ca087b2fd526ecd2a6b997996aa80bf737e
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-03-13 17:36:44 -0400

    CommentList - added add comment textarea and button. PostList- show add comment on comment button click

---
+++ src/components/CommentList.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from 'react';
+
+const CommentList = ({ comments }) => {
+  const textAreaRef = useRef(null);
+  const [comment, setComment] = useState('');
+
+  // focus cursor in textarea when user clicks comment
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.focus();
+    }
+  }, []);
+
+  const handleInput = () => {
+    if (textAreaRef.current) {
+      if (textAreaRef.current) {
+        textAreaRef.current.style.height = 'auto';
+        textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="relative">
+        <textarea
+          ref={textAreaRef}
+          value={comment}
+          rows={1}
+          onChange={(e) => {
+            setComment(e.target.value);
+            handleInput();
+          }}
+          placeholder="Add comment..."
+          className="bg-gray-50 md:w-200 text-black rounded p-2 pr-20 md:pr-30 outline-none w-full min-h-25 resize-none overflow-hidden focus:ring-0 focus:border-transparent"
+        />
+        {comment && (
+          <button className="absolute bottom-2 right-2 primary text-white px-1 md:px-3 py-1 rounded-full cursor-pointer">
+            Comment
+          </button>
+        )}
+      </div>
+      {/* <ul>
+        {comments.map((comment) => {
+          <li key={comment._id}>
+            <div
+              className={`flex flex-col px-2 border border-gray-300 rounded shadow-lg bg-gray-50 w-screen md:w-200 text-black min-h-25`}
+            >
+              <p>{comment.comment}</p>
+            </div>
+          </li>;
+        })}
+      </ul> */}
+    </>
+  );
+};
+
+export default CommentList;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,18 +2,23 @@ import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../context/UserContext';
+import CommentList from './CommentList';
 
 const PostList = ({ posts, onLike }) => {
+  const [showComments, setShowComments] = useState(false);
   const { user } = useContext(UserContext);
+
   return (
     <ul>
       {posts
         .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) // sort descending based on createdAt
         .map((post) => (
           <li key={post._id}>
-            <div className="flex flex-col border rounded shadow-lg bg-gray-50 w-screen md:w-200 text-black min-h-25 mb-2">
+            <div
+              className={`flex flex-col px-2 border border-gray-300 rounded shadow-lg bg-gray-50 w-screen md:w-200 text-black min-h-25 ${!showComments && 'mb-2'}`}
+            >
               <p className="px-2">{post.username}:</p>
               <p className="px-2">{post.post}</p>
               <div className="flex mt-auto border-t border-gray-300 justify-end gap-x-5">
@@ -32,12 +37,16 @@ const PostList = ({ posts, onLike }) => {
                   )}
                   <span className="text-xs">Like</span>
                 </button>
-                <button className="flex cursor-pointer gap-x-1 mt-auto p-1">
+                <button
+                  onClick={() => setShowComments(!showComments)}
+                  className="flex cursor-pointer gap-x-1 mt-auto p-1"
+                >
                   <FontAwesomeIcon icon={faComment} />
                   <span className="mr-5 text-xs">Comment</span>
                 </button>
               </div>
             </div>
+            {showComments && <CommentList comments={post.comments} />}
           </li>
         ))}
     </ul>
