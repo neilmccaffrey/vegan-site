97da3920a0ff5d2f6705400604ad394afa96ea91
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-03-27 16:28:28 -0400

    Being Vegan section on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,28 +21,104 @@ const Home = () => {
 
       <Header />
       {/* give container width of w-128 to center button relative to list width */}
-      <div className="flex flex-col mt-20 md:ml-10 md:w-128 md:w-96 mb-20">
-        {!displayRestaurants && (
-          <span className="self-center mb-2 text-xs">
-            Click to show all vegan/vegan friendly places within 25 miles!
-          </span>
-        )}
-        <button
-          onClick={() => setDisplayRestaurants(!displayRestaurants)}
-          disabled={loading}
-          className={`cursor-pointer rounded primary shadow-md p-2 w-fit self-center ${
-            loading ? 'opacity-50 cursor-not-allowed' : ''
-          }`}
-        >
-          {loading
-            ? 'Loading...'
-            : displayRestaurants
-              ? 'Hide Restaurants'
-              : 'Show Restaurants'}
-        </button>
-        {displayRestaurants && (
-          <RestaurantsList loading={loading} setLoading={setLoading} />
-        )}
+      <div className="flex flex-col md:flex-row mt-20">
+        <div className="flex flex-col md:ml-10 md:w-128 md:w-96 mb-20">
+          {!displayRestaurants && (
+            <span className="self-center mb-2 text-xs">
+              Click to show all vegan/vegan friendly places within 25 miles!
+            </span>
+          )}
+          <button
+            onClick={() => setDisplayRestaurants(!displayRestaurants)}
+            disabled={loading}
+            className={`cursor-pointer rounded primary shadow-md p-2 w-fit self-center ${
+              loading ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+          >
+            {loading
+              ? 'Loading...'
+              : displayRestaurants
+                ? 'Hide Restaurants'
+                : 'Show Restaurants'}
+          </button>
+          {displayRestaurants && (
+            <RestaurantsList loading={loading} setLoading={setLoading} />
+          )}
+        </div>
+        <div className="flex w-full mr-1 justify-center">
+          <div className="flex flex-col ml-2">
+            <p className="text-4xl md:text-6xl text-center">
+              🌱 Being Vegan 🌱
+            </p>
+            <p className="mt-10 font-bold text-xl">🐷 Saving Animal Lives!</p>
+            <p className="ml-3">
+              • The average vegan saves{' '}
+              <span className="font-bold">30 animal lives per month</span> (~365
+              per year).
+            </p>
+            <p className="ml-3">
+              • This includes farm animals like chickens, pigs, cows, and fish
+              that would otherwise be killed for food.
+            </p>
+            <p className="mt-5 font-bold text-xl">💧 Water Conservation!</p>
+            <p className="ml-3">
+              • A vegan diet saves{' '}
+              <span className="font-bold">
+                219,000 gallons of water per year
+              </span>{' '}
+              (~600 gallons per day).
+            </p>
+            <p className="ml-3">
+              • Producing 1 pound of beef requires{' '}
+              <span className="font-bold">1,800 gallons of water</span>, while
+              producing 1 pound of tofu takes only{' '}
+              <span className="font-bold">302 gallons.</span>
+            </p>
+            <p className="mt-5 font-bold text-xl">
+              🌳 Reducing Land Use & Deforestation!
+            </p>
+            <p className="ml-3">
+              • Animal agriculture uses{' '}
+              <span className="font-bold">77% of global farmland</span> but
+              provides only
+              <span className="font-bold">
+                {' '}
+                {`18% of the world's calories.`}
+              </span>
+            </p>
+            <p className="ml-3">
+              • Every day, a vegan saves{' '}
+              <span className="font-bold">30 sq ft of forest</span> from
+              destruction for livestock grazing and feed crops.
+            </p>
+            <p className="mt-5 font-bold text-xl">
+              🌍 Reducing Carbon Footprint!
+            </p>
+            <p className="ml-3">
+              • A vegan diet reduces carbon emissions by{' '}
+              <span className="font-bold">
+                50% compared to a meat-heavy diet.
+              </span>
+            </p>
+            <p className="ml-3">
+              • If the world went vegan, it could cut food-related greenhouse
+              gas emissions by <span className="font-bold">up to 70%.</span>
+            </p>
+            <p className="mt-5 font-bold text-xl">❤️ Health Benefits!</p>
+            <p className="ml-3">
+              • Studies show that a vegan diet can lower the risk of{' '}
+              <span className="font-bold">heart disease by 32%</span> and reduce
+              the risk of certain cancers.
+            </p>
+            <p className="ml-3">
+              • Vegans tend to have{' '}
+              <span className="font-bold">
+                lower cholesterol and blood pressure
+              </span>
+              compared to meat-eaters.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
